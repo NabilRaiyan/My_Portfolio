1c985a2b2f02e304aacf9ae71cb78aa260325ff7
NabilRaiyan <rraiyan77@gmail.com>
2024-12-14 21:44:08 +0600

    creating project

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden x-auto sm:px-10 px-5">
       <div className="font-sans">Hello form Raiyan</div>
     </main>
   );
