8dd20b0c1ac49c8261e2287c42c4652c66b3415b
NabilRaiyan <rraiyan77@gmail.com>
2024-12-15 15:47:22 +0600

    creating hero section

--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const Hero = () => {
 
     return (
-        <div className="bg-bg flex flex-row">
-            <div className="p-10">
-                
+        <div className="relative bg-bg flex flex-row pt-20">
+            <div className="absolute pr-20 right-60 p-10 text-2xl" id="intro">
+                Hello
             </div>
 
-            <div className="p-10">Hello from Hero</div>
+            <div className="relative p-10">Hello from Hero</div>
         </div>
 
 
