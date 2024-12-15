eebfdd4670926fe5c49fb9f647aebb97559cba64
NabilRaiyan <rraiyan77@gmail.com>
2024-12-15 15:19:40 +0600

    creating hero section

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,12 +1,15 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import {FaHome} from 'react-icons/fa'
+import {FaHome, FaPhone} from 'react-icons/fa'
 
 export default function Home() {
   return (
     <main className="relative bg-bg flex justify-center items-center flex-col overflow-hidden x-auto sm:px-10 px-5">
       <FloatingNav navItems={[
-        {name: 'Home', link: '/', icon: <FaHome /> }
+        {name: 'Home', link: '/', icon: <FaHome /> },
+        {name: 'My Work', link: '/', icon: < FaHome/> },
+        {name: 'About', link: '/', icon: <FaPhone /> },
+        {name: 'Contact', link: '/', icon: <FaPhone /> }
       ]} />
       <Hero />
     </main>
