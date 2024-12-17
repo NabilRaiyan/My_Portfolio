14ae6aaadeaac0a1fd990a96904166c00e0d8220
NabilRaiyan <rraiyan77@gmail.com>
2024-12-17 22:08:12 +0600

    creating tech stack section

--- my-portfolio/components/TechStack.tsx
+++ my-portfolio/components/TechStack.tsx
@@ -7,62 +7,61 @@ import {
     IconTableColumn,
   } from "@tabler/icons-react";
 
-const Skeleton = () => (
-        <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl dark:bg-dot-white/[0.1] bg-dot-black/[0.15] [mask-image:radial-gradient(ellipse_at_center,white,transparent)] border border-transparent dark:border-white/[0.15] bg-gray-900 shadow-lg shadow-black/30"></div>
-    );
-const items = [
-    {
-      title: "The Dawn of Innovation",
-      description: "Explore the birth of groundbreaking ideas and inventions.",
-      header: <Skeleton />,
-      className: "md:col-span-2",
-      icon: <IconClipboardCopy className="h-4 w-4 text-orange-500" />,
-    },
-    {
-      title: "The Digital Revolution",
-      description: "Dive into the transformative power of technology.",
-      header: <Skeleton />,
-      className: "md:col-span-1",
-      icon: <IconFileBroken className="h-4 w-4 text-orange-500" />,
-    },
-    {
-      title: "The Art of Design",
-      description: "Discover the beauty of thoughtful and functional design.",
-      header: <Skeleton />,
-      className: "md:col-span-1",
-      icon: <IconSignature className="h-4 w-4 text-orange-500" />,
-    },
-    {
-      title: "The Power of Communication",
-      description:
-        "Understand the impact of effective communication in our lives.",
-      header: <Skeleton />,
-      className: "md:col-span-2",
-      icon: <IconTableColumn className="h-4 w-4 text-orange-500" />,
-    },
-  ];
+    const Skeleton = () => (
+        <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-white dark:bg-black"></div>    );
+    const items = [
+        {
+        title: "The Dawn of Innovation",
+        description: "Explore the birth of groundbreaking ideas and inventions.",
+        header: <Skeleton />,
+        className: "md:col-span-2",
+        icon: <IconClipboardCopy className="h-4 w-4 text-orange-500" />,
+        },
+        {
+        title: "The Digital Revolution",
+        description: "Dive into the transformative power of technology.",
+        header: <Skeleton />,
+        className: "md:col-span-1",
+        icon: <IconFileBroken className="h-4 w-4 text-orange-500" />,
+        },
+        {
+        title: "The Art of Design",
+        description: "Discover the beauty of thoughtful and functional design.",
+        header: <Skeleton />,
+        className: "md:col-span-1",
+        icon: <IconSignature className="h-4 w-4 text-orange-500" />,
+        },
+        {
+        title: "The Power of Communication",
+        description:
+            "Understand the impact of effective communication in our lives.",
+        header: <Skeleton />,
+        className: "md:col-span-2",
+        icon: <IconTableColumn className="h-4 w-4 text-orange-500" />,
+        },
+    ];
 
-const TechStack = () => { 
-    return (
-        <div className="relative mt-[100px] mb-[50px] bg-black-100 w-full h-auto items-center justify-center">
-            <h1 className="text-orange-400 font-poppins text-3xl text-center">My Tech Stack</h1>
+    const TechStack = () => { 
+        return (
+            <div className="relative mt-[100px] mb-[50px] w-full h-auto items-center justify-center">
+                <h1 className="text-orange-400 font-poppins text-3xl text-center">My Tech Stack</h1>
 
 
-            <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-            {items.map((item, i) => (
-                <BentoGridItem
-                key={i}
-                title={item.title}
-                description={item.description}
-                header={item.header}
-                className={item.className}
-                icon={item.icon}
-                />
-            ))}
-            </BentoGrid>
-        </div>
-    )
+                <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+                {items.map((item, i) => (
+                    <BentoGridItem
+                    key={i}
+                    title={item.title}
+                    description={item.description}
+                    header={item.header}
+                    className={item.className}
+                    icon={item.icon}
+                    />
+                ))}
+                </BentoGrid>
+            </div>
+        )
 
-}
+    }
 
 export default TechStack
