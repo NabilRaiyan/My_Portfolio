19ba0138179db517fccf3e811609ce1c3101cd8c
NabilRaiyan <rraiyan77@gmail.com>
2024-12-22 11:20:17 +0600

    creating projects section

--- my-portfolio/components/Projects.tsx
+++ my-portfolio/components/Projects.tsx
@@ -57,7 +57,7 @@ const Projects = () => {
         Some Of My Projects
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10 mb-[100px]">
         {projects.map((project) => (
           <PinContainer
             key={project.id} // Use project.id for the key to ensure it's unique
