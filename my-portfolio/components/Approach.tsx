7c19c3c38e9cd0c8e1798f128aeb88ec2ad232e1
NabilRaiyan <rraiyan77@gmail.com>
2024-12-23 21:22:31 +0600

    creating my approach  section

--- my-portfolio/components/Approach.tsx
+++ my-portfolio/components/Approach.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React from "react";
 
-
-
-
 const Approach = ()=> {
   return (
-    <section className="w-full py-20">
-      <h1 className="heading">
+    <section className="justify-center items-center sm:justify-center sm:items-center">
+      <h1 className="text-4xl mb-10 sm:text-5xl md:text-[3.2rem] lg:text-4xl xl:text-[3rem] font-poppins font-extralight text-cyan-300 text-center mt-[100px]">
         My <span className="text-purple">Approach</span>
       </h1>
+      <div className="flex flex-row mt-[190px]">
+          <div className="">div1</div>
+          <div className="">div2</div>
+          <div className="">div3</div>
+
+
+      </div>
     </section>
   );
 }
