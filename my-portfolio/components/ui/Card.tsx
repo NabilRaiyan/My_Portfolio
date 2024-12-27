883dc3ac1e4dd2dd7dc48224aea6818ee4cbf70e
NabilRaiyan <rraiyan77@gmail.com>
2024-12-27 09:37:55 +0600

    creating approach section

--- my-portfolio/components/ui/Card.tsx
+++ my-portfolio/components/ui/Card.tsx
@@ -1,21 +1,34 @@
 import React from 'react'
 
 import * as motion from "motion/react-client"
+import Image from 'next/image';
 
 interface CardPropsType {
     title: string;
     description: string;
-    // imageLink: string;
+    imageLink: string;
 }
 
-const ApproachCard = ({title, description }:CardPropsType) => {
+const ApproachCard = ({title, description, imageLink }:CardPropsType) => {
   return (
-    <motion.div className="bg-black w-[400px] h-full p-5 rounded-sm border text-white font-poppins border-orange-200"  whileHover={{ scale: 1.2 }}>
-        {title}
-        <motion.div>
-            {description}
+    <motion.div 
+        className="flex flex-col bg-black w-[400px] h-full p-5 rounded-md border font-poppins border-orange-200"  
+        whileHover={{ scale: 1.2 }}
+        >
+    <motion.div 
+        className="flex justify-center items-center mb-4"
+        style={{ height: '200px' }}
+    >
+    <Image src={imageLink} height={200} width={200} alt="approach image" />
         </motion.div>
+    <motion.div className="text-center text-lg text-white font-semibold mb-2">
+        {title}
     </motion.div>
+    <motion.div className="text-justify text-slate-200 text-sm">
+        {description}
+    </motion.div>
+    </motion.div>
+
   )
 }
 
