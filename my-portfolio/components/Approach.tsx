6d83c770875f2f3582c2138189b416fd5b0d1262
NabilRaiyan <rraiyan77@gmail.com>
2024-12-27 09:48:52 +0600

    creating approach section

--- my-portfolio/components/Approach.tsx
+++ my-portfolio/components/Approach.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import ApproachCard from "./ui/Card";
+import approach_data from '../data/data.json'
 
 
 // Approach function
@@ -12,30 +13,17 @@ const Approach = () => {
         My <span className="text-purple">Approach</span>
       </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 mt-[160px] mb-[200px]">
-            <ApproachCard 
-                title="Planning & Strategy" 
-                description="We'll collaborate to map out your website's goals, target audience, 
-                  and key functionalities. We'll discuss things like site structure, 
-                  navigation, and content requirements."
-                  imageLink="/images/planing.svg"
-              />
-            <ApproachCard 
-                title="Development & Progress Update" 
-                description="Once we agree on the plan, I cue my lofi playlist and dive into
-                coding. From initial sketches to polished code, I keep you updated
-                every step of the way."
-                imageLink="/images/development.jpg"
-                
+            {
+              approach_data &&
+              approach_data.approach_data.map((item, idx) => (
+                <ApproachCard 
+                  key={idx} 
+                  title={item.title} 
+                  description={item.description} 
+                  imageLink={item.imageLink} 
                 />
-                
-            <ApproachCard 
-                title="Development & Launch" 
-                description="This is where the magic happens! Based on the approved design, 
-                I'll translate everything into functional code, building your website
-                from the ground up."
-                imageLink="/images/launch.jpg"
-                
-                />
+              ))
+          }
         </div>
 
     </section>
--- my-portfolio/components/ui/Card.tsx
+++ my-portfolio/components/ui/Card.tsx
@@ -15,18 +15,18 @@ const ApproachCard = ({title, description, imageLink }:CardPropsType) => {
         className="flex flex-col bg-black w-[400px] h-full p-5 rounded-md border font-poppins border-orange-200"  
         whileHover={{ scale: 1.2 }}
         >
-    <motion.div 
-        className="flex justify-center items-center mb-4"
-        style={{ height: '200px' }}
-    >
-    <Image src={imageLink} height={200} width={200} alt="approach image" />
+        <motion.div 
+            className="flex justify-center items-center mb-4"
+            style={{ height: '200px' }}
+        >
+        <Image src={imageLink} height={200} width={200} alt="approach image" />
+            </motion.div>
+        <motion.div className="text-center text-lg text-white font-semibold mb-2">
+            {title}
         </motion.div>
-    <motion.div className="text-center text-lg text-white font-semibold mb-2">
-        {title}
-    </motion.div>
-    <motion.div className="text-justify text-slate-200 text-sm">
-        {description}
-    </motion.div>
+        <motion.div className="text-justify text-slate-200 text-sm">
+            {description}
+        </motion.div>
     </motion.div>
 
   )
