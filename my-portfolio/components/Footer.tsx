d1244c05691710ab9ebabbf940f15efdfc58d7a6
NabilRaiyan <rraiyan77@gmail.com>
2024-12-28 21:58:06 +0600

    Creating footer section

--- my-portfolio/components/Footer.tsx
+++ my-portfolio/components/Footer.tsx
@@ -3,15 +3,15 @@
 
 const Footer = () => {
     return (
-        <section className="mt-10">
+        <footer className="mt-10">
             <h1 className="text-4xl mb-10 sm:text-5xl md:text-[3.2rem] leading-6 lg:text-4xl xl:text-[3rem] font-poppins font-extralight text-white text-center mt-[100px]">
                 Ready To Take You
-                <span className="block text-cyan-300 mt-4">Outside Of Digital World</span>
+                <span className="block text-cyan-300 mt-5">Outside Of Digital World</span>
             </h1>
 
            
             <div className="text-white mt-[100px]">name</div>
-        </section>
+        </footer>
     )
 }
 
