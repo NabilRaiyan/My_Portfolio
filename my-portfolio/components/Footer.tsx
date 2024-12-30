cd6a83d2b8dae52cd49e64eda2c14a3c373a58e9
NabilRaiyan <rraiyan77@gmail.com>
2024-12-30 21:27:20 +0600

    Creating footer section

--- my-portfolio/components/Footer.tsx
+++ my-portfolio/components/Footer.tsx
@@ -3,34 +3,20 @@
 import CustomButton from "./ui/CustomButton"
 import { FaLocationArrow } from 'react-icons/fa'
 
-
 const currentYear = new Date().getFullYear()
 
 const Footer = () => {
 
-
     return (
-        <footer className="mt-10 sm:mt-8 md:mt-12 lg:mt-16">
+        <footer className="mt-10 sm:mt-8 md:mt-12 lg:mt-16 px-4 sm:px-6 md:px-8 lg:px-10">
             <h1 className="text-4xl mb-10 sm:text-5xl md:text-[3.2rem] leading-6 lg:text-4xl xl:text-[3rem] font-poppins font-extralight text-white text-center mt-[100px]">
                 Ready to take
-            <span className="block text-cyan-300 mt-5">your outside digital presence</span>
+                <span className="block text-cyan-300 mt-5">your outside digital presence</span>
             </h1>
 
-           
-            <div className="text-white mt-[100px] flex flex-row gap-10">
-                <div className="w-[250px] text-clip text-wrap">
-                    <p className='text-xl text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve yours goals </p>
-                    <div className="flex justify-center items-center">
-                        <a href="mailto:raiyanalsultan@gmail.com">
-                            <CustomButton text="Let's get in touch" icon={<FaLocationArrow />} />
-                        </a>
-                    </div>
-
-                </div>
-
-
-                <div className="w-[250px] text-clip text-wrap">
-                    <p className='text-xl text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve yours goals </p>
+            <div className="text-white mt-[100px] flex flex-col sm:flex-row justify-center sm:justify-between gap-10">
+                <div className="w-full sm:w-[250px] text-clip text-wrap">
+                    <p className="text-xl text-white-200 my-5 text-center">Reach out to me today and let&apos;s discuss how I can help you achieve your goals</p>
                     <div className="flex justify-center items-center">
                         <a href="mailto:raiyanalsultan@gmail.com">
                             <CustomButton text="Let's get in touch" icon={<FaLocationArrow />} />
@@ -38,9 +24,8 @@ const Footer = () => {
                     </div>
                 </div>
 
-
-                <div className="w-[250px] text-clip text-wrap justify-center items-center">
-                    <p className='text-xl text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s discuss how I can help you achieve yours goals </p>
+                <div className="w-full sm:w-[250px] text-clip text-wrap">
+                    <p className="text-xl text-white-200 my-5 text-center">Reach out to me today and let&apos;s discuss how I can help you achieve your goals</p>
                     <div className="flex justify-center items-center">
                         <a href="mailto:raiyanalsultan@gmail.com">
                             <CustomButton text="Let's get in touch" icon={<FaLocationArrow />} />
@@ -48,9 +33,19 @@ const Footer = () => {
                     </div>
                 </div>
 
+                <div className="w-full sm:w-[250px] text-clip text-wrap">
+                    <p className="text-xl text-white-200 my-5 text-center">Reach out to me today and let&apos;s discuss how I can help you achieve your goals</p>
+                    <div className="flex justify-center items-center">
+                        <a href="mailto:raiyanalsultan@gmail.com">
+                            <CustomButton text="Let's get in touch" icon={<FaLocationArrow />} />
+                        </a>
+                    </div>
+                </div>
             </div>
 
-            <div className="text-slate-300 mb-10 sm:text-xl md:text-[1.2rem] lg:text-[1 rme] xl:text-[1rem] font-poppins font-extraligh text-center mt-[100px]">Copyright at Raiyan Al Sultan {currentYear}</div>
+            <div className="text-slate-300 mb-10 sm:text-xl md:text-[1.2rem] lg:text-[1rem] xl:text-[1rem] font-poppins font-extralight text-center mt-[100px]">
+                Copyright at Raiyan Al Sultan {currentYear}
+            </div>
         </footer>
     )
 }
