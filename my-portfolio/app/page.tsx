4977a928a6eed6e565aedb2fd5f072d2a8c91dea
NabilRaiyan <rraiyan77@gmail.com>
2025-01-03 15:24:06 +0600

    building project

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,22 +1,32 @@
+"use client"
 
 import Approach from "@/app/components/Approach";
 import Hero from "@/app/components/Hero";
 import Project from "@/app/components/Projects";
 import TechStack from "@/app/components/TechStack";
-import { FloatingNav } from "@/app/components/ui/floating-navbar";
+// import { FloatingNav } from "@/app/components/ui/floating-navbar"
 import {FaHome, FaPhone} from 'react-icons/fa'
 import Footer from "@/app/components/Footer";
+import dynamic from "next/dynamic";
 
+const FloatingNav = dynamic(
+  () => import("@/app/components/ui/floating-navbar").then((mod) => mod.FloatingNav),
+  {
+    ssr: false,
+  }
+);
 export default function Home() {
   return (
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <FloatingNav navItems={[
-        {name: 'Home', link: '#home', icon: <FaHome /> },
-        {name: 'Projects', link: '#projects', icon: < FaHome/> },
-        {name: 'My Approach', link: '#approach', icon: <FaPhone /> },
-        {name: 'About', link: '#about', icon: <FaPhone /> },
-        {name: 'Contact', link: '#contact', icon: <FaPhone /> }
-      ]} />
+      <FloatingNav
+        navItems={[
+          { name: "Home", link: "#home", icon: <FaHome /> },
+          { name: "Projects", link: "#projects", icon: <FaHome /> },
+          { name: "My Approach", link: "#approach", icon: <FaPhone /> },
+          { name: "About", link: "#about", icon: <FaPhone /> },
+          { name: "Contact", link: "#contact", icon: <FaPhone /> },
+        ]}
+      />
       <Hero />
       <TechStack />
       <Project />
